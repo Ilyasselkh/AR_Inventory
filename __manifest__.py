30ae2b72bec2cc693677b66c5ab1f0e15455441e
Ilyasselkh <109107947+Ilyasselkh@users.noreply.github.com>
2026-06-01 11:47:41 +0100

    Add __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+
+    'name': 'AR_Inventory',
+    'version':'1.0',
+    'author':'Issam Eddabbar',
+    'description':"This application is used to serve as an inventory tool for the Araymond Wherehouse",
+    'installable': True,
+
+
+    'depends': ['base'],
+    'external_dependencies': {
+        'python': ['openpyxl'],
+    },
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/inventory.xml',
+        ]
+}
